801ee1de9b69bd13523d2a852b31feaeae03cc22
Mayendra20coding <bandara.mayendra@gmail.com>
2025-12-25 20:37:20 +1300

    restaurantmanagementsystem

--- restaurantmanagementsystem.py
+++ restaurantmanagementsystem.py
@@ -26,9 +26,9 @@ class RestaurantOrderManagement:
                                                    pady=10)
         self.menu_labels = {}
         self.menu_quantities = {}
-        for i, (item, price) in enumerate(self.menu_items.items(), start=0):
+        for i, (item, price) in enumerate(self.menu_items.items(), start=1):
             label = ttk.Label(frame,
-                              text=f"{item} (${price})",
+                              text=f"{item} (${price}):",
                               font=("Helvetica", 12))
             label.grid(row=i, column=0, padx=10, pady=5)
             self.menu_labels[item] = label
@@ -45,7 +45,7 @@ class RestaurantOrderManagement:
                                             textvariable=self.currency_var,
                                             state='readonly',
                                             width=18,
-                                            values=['USD', 'INR'])
+                                            values=('USD', 'INR'))
         currency_dropdown.grid(row=len(self.menu_items)+1,
                                 column=1,
                                 padx=10,
@@ -63,9 +63,43 @@ class RestaurantOrderManagement:
         bg_width, bg_height = 800, 600
         canvas = tk.Canvas(root, width=bg_width, height=bg_height)
         canvas.pack()
-        original_image = tk.PhotoImage(file="restaurant_bg.png")
-        background_image = original_image.subsample(
-            original_image.width() // bg_width,
-            original_image.height() // bg_height)
-        canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
-        canvas.image = background_image
+       # original_image = tk.PhotoImage(file="restaurant_bg.png")
+        #background_image = original_image.subsample(
+         #   original_image.width() // bg_width,
+          #  original_image.height() // bg_height)
+        #canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
+        #canvas.image = background_image
+    def update_menu_prices(self, *args):
+        currency = self.currency_var.get()
+        symbol = '₹' if currency == 'INR' else '$'
+        rate = self.exchage_rate if currency == 'INR' else 1
+        for item, label in self.menu_items.items():
+            price=self.menu_items[item]*rate
+            label.config(text=f"{item} ({symbol}{price}):")
+    def place_order(self):
+        total_cost = 0
+        order_summary = "Order Summary:\n"
+        currency = self.currency_var.get()
+        symbol = '₹' if currency == 'INR' else '$'
+        rate = self.exchage_rate if currency == 'INR' else 1
+        for item, entry in self.menu_quantities.items():
+            quantity = entry.get()
+            if quantity.isdigit():
+                quantity = int(quantity)
+                price = self.menu_items[item] * rate
+                cost=quantity * price
+                total_cost += cost
+                if quantity > 0:
+                   order_summary+=f"{item} : {quantity} * {symbol}{price} = {symbol}{cost}\n"
+        if total_cost > 0:
+            order_summary += f"\nTotal Cost: {symbol}{total_cost}"
+            messagebox.showinfo(
+                "Order Placed",
+                order_summary)
+        else:
+            messagebox.showerror('Error','Please order at least one item.')
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RestaurantOrderManagement(root)
+    root.geometry("800x600")
+    root.mainloop()
